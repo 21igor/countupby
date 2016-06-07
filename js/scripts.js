0e7fb06a8b76e194bcdc6dce7de051e62617c84a
Igor Seleznov and Ryan Streur <igor@>
2016-06-07 10:01:35 -0700

    Fix an error where the input was a string instead of an integer. Add error messages if the user submits no numbers, negative numbers, or a second number larger than the first number.

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,21 @@ var countBy = function(d, b){
 
 $(document).ready(function() {
   $(".countUpBy").submit(function(){
+    debugger;
+    var destination = parseInt($("#countTo").val());
+    console.log("destination = " +destination);
+    var by = parseInt($("#countBy").val());
+    console.log("by = "+by);
+    if(!destination || !by || destination < 0 || by < 0){
+      alert("Please enter a positive number into both fields");
+    }
+    else if(destination<by){
+      alert("Please make sure the first number is greater than the second number");
+    }
+    else{
+      countBy(destination, by);
+      // countBy($("#countTo").val(), $("#countBy").val());
+    }
     event.preventDefault();
-    // var destination = $("#countTo").val();
-    // var by = $("#countBy").val();
-    // countBy(destination, by);
-    countBy($("#countTo").val(), $("#countBy").val());
   });
 });
